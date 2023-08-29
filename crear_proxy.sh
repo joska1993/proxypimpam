88b63fac0c1382924bf0d2b7c2a4d5177be8232e
Jose <m.joselillo93@gmail.com>
2023-08-29 11:59:30 +0200

    Update crear_proxy.sh

--- crear_proxy.sh
+++ crear_proxy.sh
@@ -1,38 +1,60 @@
 #!/bin/bash
 
-# Actualizar el sistema
+# Funciones para colores
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # Sin color
+
+# Verificar si el script se ejecuta como superusuario
+if [ "$EUID" -ne 0 ]; then
+  echo -e "${RED}Por favor, ejecuta este script como superusuario (root).${NC}"
+  exit 1
+fi
+
+# Actualizar e instalar paquetes necesarios
+echo -e "${GREEN}Actualizando e instalando paquetes necesarios...${NC}"
 sudo apt update && sudo apt upgrade -y
+sudo apt install -y squid apache2-utils curl
 
-# Instalar squid
-sudo apt install -y squid
-
-# Pedir el nombre de usuario y contraseña
+# Pedir información del usuario
 read -p "Introduce el nombre de usuario para el proxy: " USERNAME
 read -s -p "Introduce la contraseña para el proxy: " PASSWORD
 echo
 
-# Configurar Squid
-sudo bash -c "cat <<EOL >> /etc/squid/squid.conf
+# Configuración de Squid
+echo -e "${GREEN}Configurando Squid...${NC}"
+
+sudo bash -c "cat <<EOL > /etc/squid/squid.conf
 auth_param basic program /usr/lib/squid/basic_ncsa_auth /etc/squid/passwd
 auth_param basic children 5
 auth_param basic realm Proxy
 auth_param basic credentialsttl 2 hours
 acl authenticated proxy_auth REQUIRED
 http_access allow authenticated
+http_access deny all
+http_port 3128
 EOL"
 
-# Crear o actualizar el archivo de contraseña
-if [ ! -f /etc/squid/passwd ]; then
-    sudo touch /etc/squid/passwd
-fi
+# Crear o actualizar el archivo de contraseñas
+echo -e "${GREEN}Configurando usuario y contraseña...${NC}"
+sudo touch /etc/squid/passwd
 sudo htpasswd -b /etc/squid/passwd $USERNAME $PASSWORD
 
-# Reiniciar Squid para aplicar los cambios
+# Apertura del puerto 3128 en el firewall
+echo -e "${GREEN}Configurando firewall para permitir el puerto 3128...${NC}"
+sudo ufw allow 3128/tcp
+
+# Reiniciar Squid
+echo -e "${GREEN}Reiniciando Squid...${NC}"
 sudo systemctl restart squid
 
-# Mostrar datos del proxy
-echo "Tu proxy está listo"
-echo "IP: $(curl -s ifconfig.me)"
-echo "Puerto: 3128"
-echo "Usuario: $USERNAME"
-echo "Contraseña: $PASSWORD"
+# Verificar si Squid está funcionando
+if sudo systemctl is-active --quiet squid; then
+  echo -e "${GREEN}Tu proxy está listo.${NC}"
+  echo "IP: $(curl -s ifconfig.me)"
+  echo "Puerto: 3128"
+  echo "Usuario: $USERNAME"
+  echo "Contraseña: $PASSWORD"
+else
+  echo -e "${RED}Hubo un problema al configurar Squid. Por favor, verifica los logs para más detalles.${NC}"
+fi
